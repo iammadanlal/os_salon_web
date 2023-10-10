7ae49350dc896e2eb090288444c2f7f9f7d89459
Prateek Sinha <111572443+CDT-PRATEEK@users.noreply.github.com>
2023-10-10 22:50:57 +0530

    Created a Navbar for the salon website.
    Created a new component navbar with a search bar and navbar elements suitable for the salon website.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar'; 
 
 function App() {
   return (
     <div className="App">
+      <Navbar /> 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
